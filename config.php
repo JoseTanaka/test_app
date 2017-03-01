5393224fd3dc08da26da76bc88f34adf97a5f88a
Yuya Kawahara <c3tfe8ff@gmail.com>
2017-02-28 23:17:46 -0800

    lesson12 revised

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
-
 define('DSN', 'mysql:dbname=php_lesson;host=localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
--- connection.php
+++ connection.php
@@ -1,28 +1,11 @@
 <?php
-
-require_once('config.php');
+require_once('config.php')
 
 function connectPdo() {
-  try{
-    return new PDO(DSN,DB_USER,DB_PASSWORD);
-  } catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
-  }
-}
-function insertDb($data) {
-  $dbh = connectPdo();
-  $sql = 'INSERT INTO todos (todo) VALUES (:todo)';
-  $stmt = $dbh->prepare($sql);
-  $stmt->bindParam(':todo', $data, PDO::PARAM_STR);
-  $stmt->execute();
-}
-function selectAll() {
-  $dbh = connectPdo();
-  $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
-  $todo = array();
-  foreach($dbh->query($sql) as $row) {
-    array_push($todo, $row);
-  }
-  return $todo;
+	try{
+		return new PDO(DSN,DB_USER,DB_PASSWORD);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+		exit;
+	}
 }
