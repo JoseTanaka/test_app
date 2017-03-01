36c581241aa420320479016bcf2f8e3ef827586f
Yuya Kawahara <c3tfe8ff@gmail.com>
2017-02-28 23:39:50 -0800

    phpLesson12

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
-require_once('config.php')
+require_once('config.php');
 
 function connectPdo() {
 	try{
 		return new PDO(DSN,DB_USER,DB_PASSWORD);
-	} catch (PDOException $e) {
+	}	catch (PDOException $e) {
 		echo $e->getMessage();
 		exit;
 	}
